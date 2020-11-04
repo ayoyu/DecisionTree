726ff56012133cbfd5fca9d385f1478db64b49b2
ayoyu <khaliayoub9@gmail.com>
2020-11-04 22:58:28 +0100

    File not found exception

---
+++ src/Exceptions.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+#include <exception>
+
+namespace algo
+{
+class FileNotFound: public std::exception{
+public:
+    FileNotFound(std::string msg)
+        :_msg(msg)
+    {
+    }
+    virtual const char* what() const noexcept{
+        return _msg.c_str();
+    }
+private:
+    std::string _msg;
+};
+
+
+}
