8b086fb562d654ad317776a757da8414d14a95dc
ayoyu <khaliayoub9@gmail.com>
2020-11-11 02:02:58 +0100

    build the decision tree

--- src/tree/DecisionTreeClassifier.cpp
+++ src/tree/DecisionTreeClassifier.cpp
@@ -1,11 +1,199 @@
 #include "DecisionTreeClassifier.h"
+#include "../_utils.h"
 #include <iostream>
+#include <set>
+#include <cmath>
+#include <unordered_set>
 
 namespace algo
 {
-    void Node::say_hello(){
-        std::cout << "Hello Node" << std::endl;
+
+int Constraint::get_limit() const{
+    return limit;
+} 
+size_t Constraint::get_index_feature() const{
+    return index_feature;
+}
+bool Constraint::yes_record(const int& categorical_elem) const{
+    return categorical_elem < limit;
+}
+bool Constraint::true_false_record(const std::vector<int>& obs) const{
+    return obs.at(index_feature) < limit;
+}
+void Constraint::print(std::ostream& os) const {
+    os << "feature: " << index_feature << " || Q: " << "< " << limit << std::endl;
+}
+int Node::get_class_value() const {
+    return class_value;
+}
+Constraint Node::get_constrain() const {
+    return constraint;
+}
+void Node::add_left_child(std::unique_ptr<Node>&& left){
+    left_child = std::move(left);
+}
+void Node::add_right_child(std::unique_ptr<Node>&& right){
+    right_child = std::move(right);
+}
+void Node::print(std::ostream& os) const {
+    os << "level: " << level << std::endl;
+    os << constraint;
+    os << "gini: " << gini << " || " << "class: " << class_value << std::endl;
+}
+std::vector<Constraint> feature_constraints(size_t& index_feature, std::set<int>& feature_values){
+    // feature_values is a sorted set asc
+    std::vector<Constraint> constraints;
+    std::set<int>::reverse_iterator r_it;
+    for (r_it=feature_values.rbegin(); r_it!=std::prev(feature_values.rend()); r_it++){
+        constraints.push_back(Constraint(index_feature, *r_it));
+    }
+    return constraints;
+};
+
+std::unordered_map<size_t, std::vector<Constraint>> records_constraints(const std::vector<std::vector<int>>& records){
+    std::unordered_map<size_t, std::vector<Constraint>> constraints;
+    size_t nbr_features = records.at(0).size() - 1;
+    for (size_t i=0; i<nbr_features; i++){
+        std::set<int> unique_v{};
+        for (const auto &row: records){
+            unique_v.insert(row[i]);
+        }
+        std::vector<Constraint> fc = feature_constraints(i, unique_v);
+        if (!fc.empty()){
+            constraints[i] = fc;
+        } 
+    }
+    return constraints;
+};
+
+RecordsSpliter split(const Constraint& ct, const std::vector<std::vector<int>>& records){
+    std::vector<std::vector<int>> left_records;
+    std::vector<std::vector<int>> right_records;
+    std::vector<int> left_classes;
+    std::vector<int> right_classes;
+    size_t index_feature = ct.get_index_feature();
+    for (const auto &row: records){
+       if (ct.yes_record(row.at(index_feature))){
+           left_records.push_back(row);
+           left_classes.push_back(row.back());
+       }
+       else{
+           right_records.push_back(row);
+           right_classes.push_back(row.back());
+       }
+    }
+    RecordsSpliter sp{
+        left_records,
+        left_classes,
+        right_records,
+        right_classes
+    };
+    return sp;
+};
+
+double sub_gini_idex(std::vector<int>& classes){
+    size_t size = classes.size();
+    std::unordered_map<int, int> count = Counter(classes);
+    double sum_prob = 0.;
+    for (const auto &c: count){
+        sum_prob += std::pow((1. * c.second / size), 2);
+    }
+    double gini = 1. - sum_prob;
+    return gini;
+
+};
+Spliter best_split(const std::vector<std::vector<int>>& records){
+    size_t size_records = records.size();
+    std::unordered_map<size_t, std::vector<Constraint>> constraints = records_constraints(records);
+    double best_gini = 1;
+    Constraint best_c;
+    double best_left_gini, best_right_gini;
+    std::vector<std::vector<int>> best_left_records, best_right_records;
+    for (const auto &fc: constraints){
+        // testing every evaluated constraint to take the best one based on gini
+        for (const auto &c : fc.second){
+            RecordsSpliter records_spliter = split(c, records);
+            double left_gini = sub_gini_idex(records_spliter.left_classes);
+            double right_gini = sub_gini_idex(records_spliter.right_classes);
+            double gini = (1. * records_spliter.left_records.size() / size_records) * left_gini + (1. * records_spliter.right_records.size() / size_records) * right_gini; 
+            if (gini < best_gini){
+                best_gini = gini;
+                best_c = c;
+                best_left_gini = left_gini;
+                best_right_gini = right_gini;
+                best_left_records = records_spliter.left_records;
+                best_right_records = records_spliter.right_records;
+            }
+        }
+    }
+    Spliter sp{
+        best_c,
+        best_left_gini,
+        best_right_gini,
+        best_left_records,
+        best_right_records,
+        best_left_records.size(),
+        best_right_records.size()
+
+    };
+    return sp;
+
+};
+
+void DecisionTreeClassifier::_BuildTree(std::vector<std::vector<int>>& records, std::unique_ptr<Node>& node, size_t depth){
+    std::vector<int> classes;
+    for (const auto& row: records)
+        classes.push_back(row.back());
+    // check if all records have the same class
+    std::unordered_set<int> set_classes{classes.begin(), classes.end()};
+    if (set_classes.size() == 1){
+        std::unordered_set<int>::iterator it = set_classes.begin();
+        //node->set_class_value(*it);
+        node->class_value = *it;
+        return;
+    }
+    // records less than min_num => set the default class to this leafNode
+    if (records.size() < _min_num){
+        //node->set_class_value(_default_class);
+        node->class_value = _default_class;
+        return;
+    }
+    // stop if depth exceeds max_depth
+    if (!_max_depth){
+        if (depth >= _max_depth){
+            node->class_value = _default_class;
+            return;
+        }
     }
     
+    Spliter sp = best_split(records);
+    // No split left => take the majority class
+    if (sp.left_records.empty() || sp.right_records.empty()){
+        std::unordered_map<int, int> count = Counter(classes);
+        int majority_class = most_common(count).first;
+        //node->set_class_value(majority_class);
+        node->class_value = majority_class;
+        return;
+    }
+    //node->set_constraint(sp.c);
+    node->constraint = sp.c;
+    /********Left branch***************/
+    std::unique_ptr<Node> left_child = std::make_unique<Node>(sp.left_gini, depth, sp.left_nbr_samples);
+    node->add_left_child(std::move(left_child));
+    _BuildTree(sp.left_records, node->left_child, depth+1);
+    /********Right branch***************/
+    std::unique_ptr<Node> right_child = std::make_unique<Node>(sp.right_gini, depth, sp.right_nbr_samples);
+    node->add_right_child(std::move(right_child));
+    _BuildTree(sp.right_records, node->right_child, depth+1);
+
+};
+
+void DecisionTreeClassifier::fit(std::vector<std::vector<int>>& records){
+    _BuildTree(records, _root, 1);
+    std::cout << _root->constraint << std::endl;
+    std::cout << "#######################" << std::endl;
+    std::cout << *(_root->left_child) << std::endl;
+    std::cout << *(_root->right_child) << std::endl;
+};
 } // namespace algo
 
--- src/tree/DecisionTreeClassifier.h
+++ src/tree/DecisionTreeClassifier.h
@@ -1,10 +1,101 @@
 #pragma once
+#include <vector>
+#include <set>
+#include <unordered_map>
+#include <iostream>
+#include <tuple>
+#include <memory>
+#include "../_utils.h"
+
 
 namespace algo{
 
-class Node{
+class Constraint: public I_Printable{
     public:
-        void say_hello();
+        Constraint() = default;
+        Constraint(size_t idx_f, int l)
+            :index_feature(idx_f), limit(l)
+        {};
+        int get_limit() const;
+        size_t get_index_feature() const;
+        bool yes_record(const int& categorical_elem) const;
+        bool true_false_record(const std::vector<int>& obs) const;
+        virtual void print(std::ostream& os) const override;
+        virtual ~Constraint(){};
+    private:
+        size_t index_feature;
+        int limit;
+};
+
+class Node: public I_Printable{
+    public:
+        Node() = default;
+        Node(double& g, size_t& l, size_t& s)
+            :gini(g), level(l), nbr_samples(s)
+        {};
+        int get_class_value() const;
+        Constraint get_constrain() const;
+        void add_left_child(std::unique_ptr<Node>&& left);
+        void add_right_child(std::unique_ptr<Node>&& right);
+        virtual void print(std::ostream& os) const override;
+        virtual ~Node(){};
+    private:
+        Constraint constraint;
+        double gini;
+        std::unique_ptr<Node> left_child;
+        std::unique_ptr<Node> right_child;
+        size_t level{0};
+        size_t nbr_samples;
+        int class_value{NULL};
+        friend class DecisionTreeClassifier;
+};
+
+std::ostream& operator<<(std::ostream& os, const I_Printable& obj){
+    obj.print(os);
+    return os; 
+};
+std::vector<Constraint> feature_constraints(size_t& index_feature, std::set<int>& feature_values);
+std::unordered_map<size_t, std::vector<Constraint>> records_constraints(const std::vector<std::vector<int>>& records);
+
+struct RecordsSpliter{
+    std::vector<std::vector<int>> left_records;
+    std::vector<int> left_classes;
+    std::vector<std::vector<int>> right_records;
+    std::vector<int> right_classes;
+};
+
+RecordsSpliter split(const Constraint& ct, const std::vector<std::vector<int>>& records); 
+
+struct Spliter{
+    Constraint c;
+    double left_gini;
+    double right_gini;
+    std::vector<std::vector<int>> left_records;
+    std::vector<std::vector<int>> right_records;
+    size_t left_nbr_samples;
+    size_t right_nbr_samples;
+};
+
+double sub_gini_index(std::vector<int>& classes);
+
+Spliter best_split(const std::vector<std::vector<int>>& records);
+
+class DecisionTreeClassifier{
+    public:
+        DecisionTreeClassifier(size_t min_num, int default_class, size_t max_depth)
+            :_min_num(min_num), _default_class(default_class), _max_depth(max_depth)
+        {};
+        DecisionTreeClassifier(size_t min_num, int default_class)
+            :_min_num(min_num),  _default_class(default_class)
+        {};
+        void fit(std::vector<std::vector<int>>& records);
+    private:
+        void _BuildTree(std::vector<std::vector<int>>& records, std::unique_ptr<Node>& node, size_t depth);
+    private:
+        size_t _min_num;
+        int _default_class;
+        size_t _max_depth;
+        std::unique_ptr<Node> _root = std::make_unique<Node>();
 };
 
 }
