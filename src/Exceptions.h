3bd4aaf21c170f70844cdcbb9f509ffd8dcf1a7b
ayoyu <khaliayoub9@gmail.com>
2020-11-12 02:08:01 +0100

    throw exception if the input doesn't match the Model nbr_of_feature

--- src/Exceptions.h
+++ src/Exceptions.h
@@ -17,5 +17,16 @@ private:
     std::string _msg;
 };
 
+class ValueError: public std::exception{
+    public:
+        ValueError(std::string msg)
+            :_msg(msg)
+        {};
+        virtual const char* what() const noexcept{
+            return _msg.c_str();
+        }
+    private:
+        std::string _msg;
+};
 
 }
